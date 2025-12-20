30444aec84e5cfdbe2a4dbcd680a6ec27f6429f9
Abhishek Chauhan <159943733+flash-abhi@users.noreply.github.com>
2025-12-20 18:15:26 +0530

    Fix import path for AuthContext in UserContext

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { createContext, useContext, useEffect, useState } from 'react'
-import { authDataContext } from './authContext';
+import { authDataContext } from './AuthContext';
 export const userDataContext = createContext();
 function UserContext({children}) {
     let [userData,setUserData] = useState("");
